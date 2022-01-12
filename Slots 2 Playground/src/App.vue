dfc12da2933cf3344de811cdd4f0c2382f2062b6
OnSebii <lang.sebastian.006@gmail.com>
2022-01-12 09:35:19 +0100

    Aufgabe 2

---
+++ Slots 2 Playground/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container mt-5" id="app">
+    <ShowFriends :friends="myFriends">
+      <template v-slot:title>Meine Freunde</template>
+      <template v-slot:row.name="{ item }">
+        <span class="fw-bold text-primary"> {{ item.name }}</span>
+      </template>
+      <template v-slot:row.alter="{ item }">
+        {{ item.alter }}
+      </template>
+    </ShowFriends>
+  </div>
+</template>
+
+
+<script>
+import ShowFriends from '@/components/ShowFriends.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ShowFriends,
+  },
+  data() {
+    return {
+      myFriends: [
+        { name: 'Jonah', alter: 37 },
+        { name: 'Alena', alter: 28 },
+        { name: 'Milos', alter: 45 },
+      ],
+    };
+  },
+};
+</script>
---
+++ Slots 2 Playground/src/components/ShowFriends.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>
+      <slot name="title"> </slot>
+    </h3>
+    <table class="table">
+      <tr v-for="(friend, i) of friends" :key="i">
+        <td>
+          <slot name="row.name" :item="friend"> </slot>
+        </td>
+        <td>
+          <slot name="row.alter" :item="friend"> </slot>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    friends: {
+      type: Array,
+    },
+  },
+};
+</script>
